ac512287db9ed1c5e51e404cd8829967921775a5
Gabriel Oliveira de Sousa Guinda <35947429+GabrielOliv235@users.noreply.github.com>
2025-12-03 01:50:54 -0300

    newcodes

---
+++ pt3/times.py
@@ -0,0 +1,25 @@
+times_brasileiros = (
+    "Flamengo",
+    "Corinthians",
+    "São Paulo",
+    "Palmeiras",
+    "Vasco da Gama",
+    "Grêmio",
+    "Internacional",
+    "Atlético-MG",
+    "Cruzeiro",
+    "Santos",
+    "Fluminense",
+    "Botafogo"
+)
+
+print(f"Times : {times_brasileiros}")
+
+print(f"Os 3 primeiros {times_brasileiros[0:3]}")
+
+print(f"Os 5 Ultimos {times_brasileiros[8:12]}")
+
+print(f"Em ordem alfabetica {sorted(times_brasileiros)}")
+
+print(f"Possição do palmeiras {times_brasileiros.index("Palmeiras")}")
+
