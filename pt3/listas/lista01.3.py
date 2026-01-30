a2c97d0e8ffa1b2604847b1ed7f0a97366ba728c
Gabriel Oliveira de Sousa Guinda <35947429+GabrielOliv235@users.noreply.github.com>
2026-01-30 02:44:03 -0300

    Delete pt3 directory
    Dell

--- pt3/listas/lista01.3.py
+++
@@ -1,7 +0,0 @@
-valores = []
-for cont in range(0,5):
-    valores.append(int(input(f'Digite um valor para a lista {cont}: ')))
-
-for i, v in enumerate(valores) :
-    print(f'Indice {i} valor {v}...')
-print("Fim da lista")
--- pt3/tulpa/numero_entre2.py
+++
@@ -1,8 +0,0 @@
-num = ('Zero', 'Um', 'Dois', 'Tres', 'Quatro', 'Cinco')
-
-while True:
-    resp = int(input("Digite um numero de 0 à 5: "))
-    if 0 <= resp <=5:
-        print(num[resp])
-        break
-    else: print("Tente novamente")
